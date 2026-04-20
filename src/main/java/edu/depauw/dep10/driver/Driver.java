d9539751d1d6860820e0da086f59c3bbb5a7cc1a
Brian Howard <bhoward@depauw.edu>
2026-04-19 21:22:18 -0400

    Switch to new Driver class

--- src/main/java/edu/depauw/dep10/driver/Driver.java
+++ src/main/java/edu/depauw/dep10/driver/Driver.java
@@ -24,187 +24,221 @@ import edu.depauw.dep10.simulator.StepController;
 import edu.depauw.dep10.simulator.TracingController;
 
 public class Driver {
-	private static final String STD_MACROS = "/stdmacro.pep";
-	private static final String FORTH_MACROS = "/forthmacro.pep";
-	private static final String BARE_METAL_OS = "/pep10baremetal.pep";
-	private static final String FULL_OS = "/pep10os.pep";
-	private static final String FORTH_OS = "/assembler.pep";
-	private static final String PROPERTIES = "project.properties";
-	private static final String VERSION_PROP = "version";
+    private static final String PROPERTIES = "project.properties";
+    private static final String VERSION_PROP = "version";
 
-    public static void main(String[] argv) throws IOException {
-		ErrorLog log = new ErrorLog();
+    private static final String BARE_METAL_OS_HEADER = "/pep10baremetal.peph";
+    private static final String FULL_OS_HEADER = "/pep10os.peph";
+    private static final String FORTH_OS_HEADER = "/pep10forth.peph";
 
-		InitialArgs init = new InitialArgs();
-		CommandAsm asm = new CommandAsm();
-		CommandRun run = new CommandRun();
-		JCommander jc = JCommander.newBuilder()
-				.addObject(init)
-				.addCommand("asm", asm)
-				.addCommand("run", run)
-				.build();
-		jc.parse(argv);
+    private static final String BARE_METAL_OS_OBJECT = "/pep10baremetal.pepo";
+    private static final String FULL_OS_OBJECT = "/pep10os.pepo";
+    private static final String FORTH_OS_OBJECT = "/pep10forth.pepo";
 
-		if (init.showVersion) {
-		    var properties = new Properties();
-		    properties.load(Driver.class.getClassLoader().getResourceAsStream(PROPERTIES));
-		    System.out.println(properties.getProperty(VERSION_PROP));
-		} else if (init.showHelp || jc.getParsedCommand() == null) {
+    private static final String STD_MACROS = "/stdmacro.pep";
+
+    public static void main(String[] args) {
+        InitialArgs init = new InitialArgs();
+        CommandAsm asm = new CommandAsm();
+        CommandRun run = new CommandRun();
+        JCommander jc = JCommander.newBuilder()
+                .addObject(init)
+                .addCommand("asm", asm)
+                .addCommand("run", run)
+                .build();
+        jc.parse(args);
+
+        if (init.showVersion) {
+            try {
+                var properties = new Properties();
+                properties.load(Driver.class.getClassLoader().getResourceAsStream(PROPERTIES));
+                System.out.println(properties.getProperty(VERSION_PROP));
+            } catch (IOException e) {
+                System.err.println("Unable to load resource " + PROPERTIES);
+            }
+        } else if (init.showHelp || jc.getParsedCommand() == null) {
             jc.usage();
         } else {
-			switch (jc.getParsedCommand()) {
-			case "asm":
-				if (asm.showHelp) {
-					jc.usage("asm");
-				} else {
-					doAsm(asm, log);
-				}
-				break;
-			case "run":
-				if (run.showHelp) {
-					jc.usage("run");
-				} else {
-					doRun(run, log);
-				}
-				break;
-			default:
-			    jc.usage();
-			    break;
-			}
-		}
-	}
+            var log = new ErrorLog();
 
-	private static void doAsm(CommandAsm asm, ErrorLog log) {
-	    Sources sources = new Sources();
-	    
-	    // First add standard macros
-	    sources.addResource(STD_MACROS, log); // TODO option to substitute?
-	    
-	    // After loading macros, set option to generate OS listing if desired
-	    sources.setVisibleResources(asm.osListing);
-	    
-	    // Next add the distinguished source file, if any
-	    if (asm.sourceFile != null) {
-	        sources.addFile(asm.sourceFile, log);
-	    }
-	    
-	    // Add any other source files from the command line
-	    for (var name : asm.sourceList) {
-	        sources.addFile(name, log);
-	    }
-	    
-	    // If no sources specified, add standard input
-	    if (asm.sourceFile == null && asm.sourceList.isEmpty()) {
-	        sources.addStdIn();
-	    }
-	    
-	    // Add the appropriate OS
-		if (asm.bareMetal && asm.osName != null) {
-			log.error("Bare metal excludes specifying OS.");
-			return;
-		}
-		
-		if (asm.bareMetal) {
-		    sources.addResource(BARE_METAL_OS, log);
-		} else if (asm.osName != null) {
-		    if (asm.osName.equals("FORTH")) {
-		        // special case for the FORTH OS
-		        sources.addResource(FORTH_MACROS, log);
-		        sources.addResource(FORTH_OS, log);
-		    } else {
-		        sources.addFile(asm.osName, log);
-		    }
-		} else {
-		    sources.addResource(FULL_OS, log);
-		}
+            switch (jc.getParsedCommand()) {
+            case "asm":
+                if (asm.showHelp) {
+                    jc.usage("asm");
+                } else {
+                    doAsm(asm, log);
+                }
+                break;
+            case "run":
+                if (run.showHelp) {
+                    jc.usage("run");
+                } else {
+                    doRun(run, log);
+                }
+                break;
+            default:
+                jc.usage();
+                break;
+            }
+        }
+    }
 
-		var preprocessor = new Preprocessor(log);
-		Result result = null;
-		
-		if (log.noErrors()) {
-    		var lines = preprocessor.preprocess(sources);
-    		// all macros and includes have been expanded
-    		
-    		var assembler = new Assembler(log);
-    		result = assembler.assemble(lines);
-    		
-    		if (asm.objectFile != null) {
-    		    try (var out = new BufferedWriter(new FileWriter(asm.objectFile))) {
-    		        out.write(result.toObjectFile());
-    		    } catch (IOException e) {
+    private static void doAsm(CommandAsm asm, ErrorLog log) {
+        Sources sources = new Sources();
+
+        // Include header for selected OS
+        if (asm.bareMetal) {
+            if (asm.osName != null) {
+                log.error("Bare metal excludes specifying OS.");
+            }
+            sources.addResource(BARE_METAL_OS_HEADER, log);
+        } else if (asm.osName != null) {
+            if (asm.osName.equalsIgnoreCase("FULL")) {
+                sources.addResource(FULL_OS_HEADER, log);
+            } else if (asm.osName.equalsIgnoreCase("FORTH")) {
+                sources.addResource(FORTH_OS_HEADER, log);
+            } else {
+                sources.addFile(asm.osName + ".peph", log);
+            }
+        } else if (!asm.sysMode) {
+            sources.addResource(FULL_OS_HEADER, log);
+        } else {
+            // assembling an OS in system mode; don't include
+            // an OS header unless specifically requested,
+            // but do include the standard macros
+            sources.addResource(STD_MACROS, log);
+        }
+
+        // Next add the distinguished source file, if any
+        if (asm.sourceFile != null) {
+            sources.addFile(asm.sourceFile, log);
+        }
+
+        // Add any other source files from the command line
+        for (var name : asm.sourceList) {
+            sources.addFile(name, log);
+        }
+
+        // If no sources specified, add standard input
+        if (asm.sourceFile == null && asm.sourceList.isEmpty()) {
+            sources.addStdIn();
+        }
+        
+        var preprocessor = new Preprocessor(log);
+        Result result = null;
+        
+        if (log.noErrors()) {
+            var lines = preprocessor.preprocess(sources);
+            // all macros and includes have been expanded
+            
+            var assembler = new Assembler(log);
+            result = assembler.assemble(lines);
+            
+            if (asm.objectFile != null) {
+                try (var out = new BufferedWriter(new FileWriter(asm.objectFile))) {
+                    out.write(result.toObjectFile());
+                } catch (IOException e) {
                     log.error(e.getMessage());
                 }
-    		} else {
-    		    System.out.println(result.toObjectFile());
-    		}
-    		
-    		if (asm.listingFile != null) {
-    		    try (var out = new PrintWriter(new BufferedWriter(new FileWriter(asm.listingFile)))) {
-    		        result.printListing(out);
-    		    } catch (IOException e) {
-    		        log.error(e.getMessage());
-    		    }
-    		}
-		}
-		
-		if (asm.errorFile != null) {
-		    try (var out = new PrintWriter(new BufferedWriter(new FileWriter(asm.errorFile)))) {
-		        for (var message : log.getMessages()) {
-		            out.println(message);
-		        }
-		        
-		        if (result != null) {
-		            result.printErrors(out);
-		        }
-		    } catch (IOException e) {
-		        System.err.println("Error writing error file!");
-		    }
-		} else {
-		    for (var message : log.getMessages()) {
-		        System.err.println(message);
-		    }
-		}
-	}
+            } else {
+                System.out.println(result.toObjectFile());
+            }
+            
+            if (asm.listingFile != null) {
+                try (var out = new PrintWriter(new BufferedWriter(new FileWriter(asm.listingFile)))) {
+                    result.printListing(out);
+                } catch (IOException e) {
+                    log.error(e.getMessage());
+                }
+            }
+            
+            if (asm.exportFile != null) {
+                // Generate a header file
+                try (var out = new PrintWriter(new BufferedWriter(new FileWriter(asm.exportFile)))) {
+                    preprocessor.printMacros(out);
+                    result.printHeader(out);
+                } catch (IOException e) {
+                    log.error(e.getMessage());
+                }
+            }
+        }
+        
+        if (asm.errorFile != null) {
+            try (var out = new PrintWriter(new BufferedWriter(new FileWriter(asm.errorFile)))) {
+                for (var message : log.getMessages()) {
+                    out.println(message);
+                }
+                
+                if (result != null) {
+                    result.printErrors(out);
+                }
+            } catch (IOException e) {
+                System.err.println("Error writing error file!");
+            }
+        } else {
+            for (var message : log.getMessages()) {
+                System.err.println(message);
+            }
+        }
+    }
 
     private static void doRun(CommandRun run, ErrorLog log) {
-	    State state = (run.trace == null) ? new State() : new DebugState();
-	    
-	    for (var param : run.parameters) {
-	        state.loadFile(param);
-	    }
-	    
-	    state.setInput(run.consoleIn);
-	    state.setOutput(run.consoleOut);
-	    state.setError(run.errOut);
-	    
-	    Simulator sim = new Simulator(state);
-	    
-	    Controller control = new PlainController();
-	    if (run.max > 0) {
-	        control = new StepController(control, run.max);
-	    }
-	    
-	    TracingController tc = null;
-	    if (run.trace != null) {
-	        tc = new TracingController(control);
-	        control = tc;
-	    }
-	    
-	    // TODO for interactive use, also support breakpoints and single-stepping
-	    sim.run(control);
-	    
-	    if (run.memDump != null) {
-	        state.dump(run.memDump);
-	    }
-	    
-	    if (run.trace != null) {
-	        try {
-	            var output = new PrintStream(new File(run.trace));
-	            tc.printTrace(output);
-	            output.close();
-	        } catch (FileNotFoundException e) {
-	            System.err.println("Unable to open tracing output: " + run.trace);
-	        }
-	    }
-	}
+        State state = (run.trace == null) ? new State() : new DebugState();
+        
+        for (var param : run.parameters) {
+            state.loadFile(param);
+        }
+        
+        // load object code for selected OS
+        if (run.bareMetal) {
+            if (run.osName != null) {
+                log.error("Bare metal excludes specifying OS.");
+            }
+            state.loadResource(BARE_METAL_OS_OBJECT);
+        } else if (run.osName != null) {
+            if (run.osName.equalsIgnoreCase("FULL")) {
+                state.loadResource(FULL_OS_OBJECT);
+            } else if (run.osName.equalsIgnoreCase("FORTH")) {
+                state.loadResource(FORTH_OS_OBJECT);
+            } else {
+                state.loadFile(run.osName + ".pepo");
+            }
+        } else {
+            state.loadResource(FULL_OS_OBJECT);
+        }
+
+        state.setInput(run.consoleIn);
+        state.setOutput(run.consoleOut);
+        state.setError(run.errOut);
+        
+        Simulator sim = new Simulator(state);
+        
+        Controller control = new PlainController();
+        if (run.max > 0) {
+            control = new StepController(control, run.max);
+        }
+        
+        TracingController tc = null;
+        if (run.trace != null) {
+            tc = new TracingController(control);
+            control = tc;
+        }
+        
+        // TODO for interactive use, also support breakpoints and single-stepping
+        sim.run(control);
+        
+        if (run.memDump != null) {
+            state.dump(run.memDump);
+        }
+        
+        if (run.trace != null) {
+            try {
+                var output = new PrintStream(new File(run.trace));
+                tc.printTrace(output);
+                output.close();
+            } catch (FileNotFoundException e) {
+                System.err.println("Unable to open tracing output: " + run.trace);
+            }
+        }
+    }
 }
